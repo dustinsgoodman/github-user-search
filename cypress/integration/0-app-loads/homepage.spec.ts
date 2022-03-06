e070d70b22be53828acffcd16b179f48760d0a0b
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-03-06 13:31:43 -0600

    test: add cypress configs and first test

---
+++ cypress/integration/0-app-loads/homepage.spec.ts
@@ -0,0 +1,11 @@
+describe('app homepage loads', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('should render the home page and display a message', () => {
+    cy.get('h1').contains('Welcome');
+  });
+});
+
+export {};
