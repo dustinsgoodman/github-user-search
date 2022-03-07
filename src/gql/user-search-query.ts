03b23703358699a5c1cc4f39582e4709ec1a89ee
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-03-06 21:56:24 -0600

    feat: add UserSearch query

---
+++ src/gql/user-search-query.ts
@@ -0,0 +1,56 @@
+import { gql } from 'graphql-request';
+
+export const USER_SEARCH_QUERY = gql`
+  query UserSearch(
+    $query: String!
+    $first: Int
+    $last: Int
+    $before: String
+    $after: String
+  ) {
+    search(
+      query: $query
+      type: USER
+      first: $first
+      last: $last
+      before: $before
+      after: $after
+    ) {
+      userCount
+      nodes {
+        ... on Organization {
+          avatarUrl
+          description
+          orgEmail: email
+          location
+          login
+          name
+          url
+        }
+        ... on User {
+          avatarUrl
+          userEmail: email
+          followers {
+            totalCount
+          }
+          following {
+            totalCount
+          }
+          location
+          login
+          name
+          starredRepositories {
+            totalCount
+          }
+          url
+        }
+      }
+      pageInfo {
+        startCursor
+        endCursor
+        hasPreviousPage
+        hasNextPage
+      }
+    }
+  }
+`;
