5a6eb55830cd3b8f82f17da921f8b7fed5a26b92
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-03-12 22:47:47 -0600

    feat: add search hook

---
+++ src/hooks/use-search-results/useSearchResults.tsx
@@ -0,0 +1,18 @@
+import type { SearchInputs } from 'components/search-form';
+import { useQuery } from 'react-query';
+import type { APISearchResponse } from 'types/search';
+
+const getSearchResults = async ({ username }: SearchInputs) => {
+  const queryParams = new URLSearchParams({ username }).toString();
+  const response = await fetch(`/api/search?${queryParams}`, {});
+  const data: APISearchResponse = await response.json();
+  return data;
+};
+
+const useSearchResults = (searchInputs: SearchInputs) => {
+  return useQuery([], () => getSearchResults(searchInputs), {
+    enabled: Boolean(searchInputs.username),
+  });
+};
+
+export default useSearchResults;
