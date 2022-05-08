966ab9d6cf561ed127f6165eda89950e4bb68fe8
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-05-07 22:44:30 -0500

    test: add search tests and fix accessibility issues

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,8 +1,11 @@
+import { waitFor } from '@testing-library/react';
 import { getPage } from 'next-page-tester';
-import { screen } from '@testing-library/react';
+import { act, fireEvent, screen, mockRouter } from 'test/utils';
+
+jest.mock('next/router', () => require('next-router-mock'));
 
 describe('Home', () => {
-  it('renders a heading', async () => {
+  it('renders a heading and perform search', async () => {
     const { render } = await getPage({
       route: '/',
       wrappers: 'test/wrappers.tsx',
@@ -14,5 +17,23 @@ describe('Home', () => {
     });
 
     expect(heading).toBeInTheDocument();
+
+    const searchInput = screen.getByRole('textbox', {
+      name: 'username',
+    });
+    const searchButton = screen.getByRole('button', {
+      name: 'Submit',
+    });
+
+    act(() => {
+      fireEvent.change(searchInput, { target: { value: 'reactjs' } });
+    });
+
+    await waitFor(() => {
+      fireEvent.click(searchButton);
+    });
+
+    expect(mockRouter.pathname).toEqual('/results/reactjs');
+    expect(mockRouter.asPath).toEqual('/results/reactjs');
   });
 });
