a795b7b45d1397e9d126a0e06a42297815c09948
Dustin Goodman <dustin.s.goodman@gmail.com>
2022-05-07 21:23:21 -0500

    feat: add global hydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import '../styles/globals.css';
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 function AppRoot({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
